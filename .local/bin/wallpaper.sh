28ca4e32bbbdf68b3027c1cafad37c79b31a6873
Benjamin Schmidt <benndt.dev@gmail.com>
2026-02-15 11:51:34 +0100

    Add midnight and random deer to wallpaper script

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -1,15 +1,40 @@
-while ! pidof "hyprpaper" > /dev/null; do
+_get_suffix() {
+  randomNumber=$(shuf -i 1-100 -n 1)
+
+  if [[ $randomNumber -lt 50 ]]; then
+    echo '-deer'
+  fi
+}
+
+_get_daytime_wallpaper() {
+  suffix=$(_get_suffix)
+  hour=$(date +%-H)
+
+  if [[ $hour -ge 23 || $hour -lt 7 ]]; then
+    echo "midnight"
+  elif [[ $hour -ge 20 && $hour -lt 23 ]]; then
+    echo "evening${suffix}"
+  elif [[ $hour -ge 16 && $hour -lt 20 ]]; then
+    echo "sunset${suffix}"
+  elif [[ $hour -ge 7 ]]; then
+    echo "sunrise${suffix}"
+  fi
+}
+
+_wait_for_hyprpaper() {
+  while ! pidof "hyprpaper" > /dev/null; do
+    sleep 1
+  done
+
   sleep 1
-done
+}
 
-sleep 1
+_change_wallpaper() {
+  _wait_for_hyprpaper
 
-hour=$(date +%-H)
+  daytime_wallpaper=$(_get_daytime_wallpaper)
+  
+  hyprctl hyprpaper wallpaper ",~/images/wallpaper/lakeside-${daytime_wallpaper}.png"
+}
 
-if [[ $hour -ge 20 || $hour -lt 6 ]]; then
-  hyprctl hyprpaper wallpaper ',~/images/wallpaper/lakeside-evening-deer.png'
-elif [[ $hour -ge 16 && $hour -lt 20 ]]; then
-  hyprctl hyprpaper wallpaper ',~/images/wallpaper/lakeside-sunset-deer.png'
-elif [[ $hour -ge 6 ]]; then
-  hyprctl hyprpaper wallpaper ',~/images/wallpaper/lakeside-sunrise-deer.png'
-fi
+_change_wallpaper
