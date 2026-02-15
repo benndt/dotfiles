ee07fab98638016f27d5e6fc96940be1147d3912
Benjamin Schmidt <benndt.dev@gmail.com>
2026-02-15 09:11:06 +0100

    Fix wallpaper sunset condition

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -3,11 +3,12 @@ while ! pidof "hyprpaper" > /dev/null; do
 done
 
 sleep 1
+
 hour=$(date +%-H)
 
 if [[ $hour -ge 20 || $hour -lt 6 ]]; then
   hyprctl hyprpaper wallpaper ',~/images/wallpaper/lakeside-evening-deer.png'
-elif [[ $hour -ge 16 || $hour -lt 20 ]]; then
+elif [[ $hour -ge 16 && $hour -lt 20 ]]; then
   hyprctl hyprpaper wallpaper ',~/images/wallpaper/lakeside-sunset-deer.png'
 elif [[ $hour -ge 6 ]]; then
   hyprctl hyprpaper wallpaper ',~/images/wallpaper/lakeside-sunrise-deer.png'
