51f170d58f91a7b2ceeb36d2276ab3dc845b35f5
Benjamin Schmidt <benndt@tuta.com>
2026-03-07 10:19:34 +0100

    Change qr script timeframe

--- .local/bin/qr.sh
+++ .local/bin/qr.sh
@@ -3,7 +3,7 @@
 _open_qr() {
   hour=$(date +%-H)
 
-  if [[ $hour -ge 5 && $hour -le 7 ]]; then
+  if [[ $hour -ge 5 && $hour -le 6 ]]; then
     hyprctl dispatch -- exec gwenview --spotlight ~/images/qr/catppuccin_alarm_clock.png
   fi
 }
